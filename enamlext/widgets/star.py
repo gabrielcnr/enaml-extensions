154f1856798a2765867e925d2a675c3cdb466b7a
gabriel.reis <gabrielcnr@gmail.com>
2015-06-30 22:05:53 +0100

    StarRating enaml control and example.

---
+++ enamlext/widgets/star.py
@@ -0,0 +1,54 @@
+from atom.api import (
+    Typed, ForwardTyped, Int, observe, set_default
+)
+
+from enaml.core.declarative import d_
+
+from enaml.widgets.control import Control, ProxyControl
+
+
+
+class ProxyStarRating(ProxyControl):
+    """ The abstract definition of a proxy StarRatingWidget object.
+
+    """
+    #: A reference to the Hello declaration.
+    declaration = ForwardTyped(lambda: StarRating)
+
+    def set_stars(self, stars):
+        raise NotImplementedError
+
+    def set_rating(self, rating):
+        raise NotImplementedError
+
+
+
+class StarRating(Control):
+    """ Enaml declarative control for giving a Star Rating widget.
+
+    """
+    stars = d_(Int(default=5))
+    rating = d_(Int())
+
+    #: A reference to the ProxyStarRating object.
+    proxy = Typed(ProxyStarRating)
+
+    #--------------------------------------------------------------------------
+    # Observers
+    #--------------------------------------------------------------------------
+    @observe('stars', 'rating')
+    def _update_proxy(self, change):
+        """ An observer which sends the state change to the proxy.
+
+        """
+        # The superclass implementation is sufficient.
+        super(StarRating, self)._update_proxy(change)
+
+
+def star_rating_factory():
+    from enamlext.qt.qt_star import QtStarRating
+    return QtStarRating
+
+
+from enaml.qt.qt_factories import QT_FACTORIES
+QT_FACTORIES['StarRating'] = star_rating_factory
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enaml-extensions',
+    version='0.1',
+    author='Gabriel Reis',
+    author_email='gabrielcnr@gmail.com',
+    requires=['enaml'],
+    packages=find_packages(),
+)
