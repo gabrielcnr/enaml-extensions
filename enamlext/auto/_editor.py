3d2f5e2be2b315cbd5588ea9fa038b206974e8cb
Gabriel Reis <gabriel.reis@gazprom-mt.com>
2015-08-13 13:53:52 +0100

    Added support to datetime and int

--- enamlext/auto/_editor.py
+++ enamlext/auto/_editor.py
@@ -1,7 +1,11 @@
-from atom.api import Atom, Value
-from enaml.widgets.api import Form, Label
+from atom.api import Atom, Value, Typed
+from enaml.widgets.api import Form, Label, DatetimeSelector
 from enaml.core.declarative import d_
 from enamlext.widgets import Field
+import datetime
+import enaml
+with enaml.imports():
+    from enaml.stdlib.fields import IntField
 
 
 def connect(model, model_attr, view, view_attr):
@@ -14,7 +18,10 @@ def connect(model, model_attr, view, view_attr):
                  lambda change: setattr(model, model_attr, change['value']))
 
     # Initially the view should be sync'd with the model
-    setattr(view, view_attr, getattr(model, model_attr))
+    try:
+        setattr(view, view_attr, getattr(model, model_attr))
+    except Exception, ex:
+        import pdb ;pdb.set_trace()
 
 
 def create_editor(obj, form=None):
@@ -26,6 +33,10 @@ def create_editor(obj, form=None):
 
     # get the members
     for member_name, member_descriptor in klass.members().iteritems():
+        # by default we are skipping members starting with _
+        if member_name.startswith('_'):
+            continue
+
         m = member_descriptor.metadata or {}
         if m.get('exclude', False):
             continue
@@ -33,10 +44,20 @@ def create_editor(obj, form=None):
         label_text = m.get('label', member_name)
         label = Label(text=label_text)
 
-        field = Field()
-        form.insert_children(None, [label, field])
+        widget = Field()
+        model_attr = 'text'
+        if isinstance(member_descriptor, Typed):
+            type_ = member_descriptor.validate_mode[1]
+            if issubclass(type_, datetime.date):
+                widget = DatetimeSelector(calendar_popup=True)
+                model_attr = 'datetime'
+            elif issubclass(type_, int):
+                widget = IntField()
+                model_attr = 'value'
 
-        connect(obj, member_name, field, 'text')
+        form.insert_children(None, [label, widget])
+
+        connect(obj, member_name, widget, model_attr)
 
     return form
 
