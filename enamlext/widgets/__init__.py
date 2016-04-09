3bfabdfdfd9ef61c4a538703bd1e95d5ad1ad789
gabriel.reis <gabrielcnr@gmail.com>
2016-04-09 21:52:59 +0100

    NEW: TextInputDialog + ask_text()

--- enamlext/widgets/__init__.py
+++ enamlext/widgets/__init__.py
@@ -1,4 +1,4 @@
 from .table import Table, Column
 from .star import StarRating
 from .field import Field
-from ._shortcuts import ask_yes_no
+from ._shortcuts import ask_yes_no, ask_text
--- enamlext/widgets/_shortcuts.py
+++ enamlext/widgets/_shortcuts.py
@@ -1,7 +1,16 @@
 from enaml.stdlib.message_box import question
+import enaml
 
 
 def ask_yes_no(title, message, parent=None):
     button = question(parent, title, message)
     return button.text == 'Yes'
 
+
+def ask_text(title, message, text='', parent=None):
+    with enaml.imports():
+        from enamlext.widgets._text_input_dialog import TextInputDialog
+        dlg = TextInputDialog(parent=parent, title=title, message=message,
+                              text=text)
+        if dlg.exec_():
+            return dlg.text
