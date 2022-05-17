365139fa647faa7ec61019f6c18245345bd57c4f
Gabriel Reis <gabrielcnr@gmail.com>
2022-05-17 16:58:16 +0100

    Registering the factories

--- src/enamlext/widgets/__init__.py
+++ src/enamlext/widgets/__init__.py
@@ -2,3 +2,20 @@ from .table import Table
 from .star import StarRating
 from .field import Field
 from ._shortcuts import ask_yes_no, ask_text
+
+
+from enaml.qt.qt_factories import QT_FACTORIES
+
+# Injecting the factories to be used by the ProxyResolver
+# TODO: should this be here?
+def table_factory():
+    from enamlext.qt.qt_table import QtTable
+    return QtTable
+
+
+QT_FACTORIES.update(
+    Table=table_factory,
+)
+
+del QT_FACTORIES
+del table_factory
