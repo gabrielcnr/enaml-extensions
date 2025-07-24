b64d296f4978ff8a8a4eb83d261e98d9caaddde0
Gabriel Reis <gabrielcnr@gmail.com>
2025-07-24 16:16:00 +0100

    DataFrame ticking fixes and instrumentation (#10)
    * enhancements
    
    * new example
    
    * better
    
    * df index to view index
    
    * flag for instrumentation
    
    ---------
    
    Co-authored-by: Reis, Gabriel <gabriel.reis@uk.mlp.com>

--- src/enamlext/qt/qt_dataframe.py
+++ src/enamlext/qt/qt_dataframe.py
@@ -2,6 +2,8 @@ import logging
 import threading
 from typing import Callable
 
+import time
+import numpy as np
 import pandas as pd
 
 
@@ -9,11 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 def _monitor_df_changes(df_proxy):
-    logger.debug('Starting DataFrameProxy thread for monitoring changes and refreshing ticking cells '
-                 f'{threading.current_thread()}')
-
-    import time
-    import numpy as np
+    if df_proxy.instrumentation_enabled:
+        logger.info('Starting DataFrameProxy thread for monitoring changes and refreshing ticking cells '
+                    f'{threading.current_thread()}')
+    
     from enaml.application import deferred_call
 
     current_values = df_proxy.values
@@ -21,13 +22,25 @@ def _monitor_df_changes(df_proxy):
 
     while df_proxy.is_active():
         time.sleep(interval)
+
+        if df_proxy.instrumentation_enabled:
+            t0 = time.perf_counter()
+
         new_values = df_proxy.df.values.copy()
-        row_indexes, col_indexes = np.where(current_values != new_values)
+
+        row_indexes, col_indexes = np.where(~((current_values == new_values) | (pd.isna(current_values) & pd.isna(new_values))))
+
         if len(row_indexes):
             deferred_call(df_proxy.update_values_and_refresh_cells, new_values, row_indexes, col_indexes)
+
         current_values = new_values
 
-    logger.debug(f'Thread died: DataFrameProxy ticking monitor {threading.current_thread()}')
+        if df_proxy.instrumentation_enabled:
+            elapsed = time.perf_counter() - t0
+            logger.info(f'It took {t1 -t0:.3f} s inside the monitoring thread')
+
+    if df_proxy.instrumentation_enabled:
+        logger.info(f'Thread died: DataFrameProxy ticking monitor {threading.current_thread()}')
 
 
 class DataFrameProxy:
@@ -35,7 +48,8 @@ class DataFrameProxy:
                  df: pd.DataFrame,
                  *,
                  tick_interval_ms: int = 0,
-                 refresh_cells_callback: Callable[[list, list], None] | None = None):
+                 refresh_cells_callback: Callable[[list, list], None] | None = None,
+                 instrumentation_enabled: bool = False):
         if tick_interval_ms > 0 and refresh_cells_callback is None:
             raise ValueError('You must pass a callback to handle refreshing cells when you pass '
                              'a tick_interval_ms > 0.')
@@ -46,6 +60,7 @@ class DataFrameProxy:
         self.df = df
         self.tick_interval_ms = tick_interval_ms
         self.refresh_cells_callback = refresh_cells_callback
+        self.instrumentation_enabled = instrumentation_enabled
         self._is_active = True
         if self.is_ticking:
             import threading
@@ -56,8 +71,16 @@ class DataFrameProxy:
 
     def update_values_and_refresh_cells(self, new_values, row_indexes, col_indexes):
         # must be called in the main thread!
+        if self.instrumentation_enabled:
+            t0 = time.perf_counter()
+
         self.values = new_values
         self.refresh_cells_callback(row_indexes, col_indexes)
+
+        if self.instrumentation_enabled:
+            elapsed = time.perf_counter() - t0
+            changed_col_names = {i: self.df.columns[i] for i in set(col_indexes)}
+            logger.info(f'It took {elapsed:.3f} s inside the main thread refreshing {len(row_indexes)} cells - modified cols: {changed_col_names}')
 
     @property
     def is_ticking(self):
